986eaea2ba94718b25cbeb65ea3ed15761415101
Adel <80000156+Cyberft-pdf@users.noreply.github.com>
2023-09-17 17:25:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import random 
+
+def play():
+    user = input("Jaká je tvoje volba: R pro kámen, p pro papír, s pro nuzky\n")
+    computer = random.choice(["r", "p", "s"])
+    if user == computer:
+        return "Je\" remíza"
+
+    if is_win(user, computer):
+        return "Ty si vyhrál"
+    print("ahoj3")
+    return "prohrál si:("
+
+
+def is_win(player, opponent):
+    if (player == "r" and opponent == "s") or (player == "s" and opponent == "p") \
+        or (player == "p" and opponent == "r"):
+
+        return True 
+
+print(play())
 
